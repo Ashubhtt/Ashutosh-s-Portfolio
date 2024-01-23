8b17dd928ad6afc29b6d4696d1f0aae181598667
Ashutosh Bhatt <98254150+Ashubhtt@users.noreply.github.com>
2024-01-23 11:49:31 +0530

    Delete Ashu's Port directory

--- Ashu's Port/script.js
+++
@@ -1,9 +0,0 @@
-const toggle_btn = document.getElementById('checkbox');
-
-toggle_btn.addEventListener('change', () => {
-    if (toggle_btn.checked) {
-        document.body.classList.add('dark-mode');
-    } else {
-        document.body.classList.remove('dark-mode');
-    }
-});
